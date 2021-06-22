f5778025d9db019c2f97ed6b6550949ab80738e4
autsada <autsada.t@gmail.com>
2021-06-22 16:49:28 +0700

    Updated cookies config for prod

--- src/utils/tokenHandler.ts
+++ src/utils/tokenHandler.ts
@@ -2,12 +2,17 @@ import jwt from 'jsonwebtoken'
 import { Response } from 'express'
 
 export const createToken = (userId: string, tokenVersion: number) =>
-  jwt.sign({ userId, tokenVersion }, process.env.COOKIE_SECRET!, {
-    expiresIn: '15d',
-  })
+    jwt.sign({ userId, tokenVersion }, process.env.COOKIE_SECRET!, {
+        expiresIn: '15d',
+    })
 
 export const sendToken = (res: Response, token: string) =>
-  res.cookie(process.env.COOKIE_NAME!, token, { httpOnly: true })
+    res.cookie(process.env.COOKIE_NAME!, token, {
+        httpOnly: true,
+        domain: process.env.FRONTEND_URI,
+        secure: true,
+        sameSite: 'none',
+    })
 
 export const verifyToken = (token: string) =>
-  jwt.verify(token, process.env.COOKIE_SECRET!)
+    jwt.verify(token, process.env.COOKIE_SECRET!)
